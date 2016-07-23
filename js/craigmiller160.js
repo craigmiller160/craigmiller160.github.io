d307f488e9f1a86d36a23c38da1ed55cedb9649b
Craig Miller <craigmiller160@gmail.com>
2016-07-23 17:32:20 -0400

    Moved css and js code to external files, and started work on skills.html page

---
+++ js/craigmiller160.js
@@ -0,0 +1,31 @@
+/* CraigMiller160 GitHub Page JavaScript */
+function contentChange(event){
+    if(event.type === "click"){
+        var page = $(this).attr("page");
+        var target = $(this).attr("target");
+        
+        $("[target='" + target + "']").parent("li").removeClass("active");
+        
+        if($(this).parent().prop("nodeName") === "LI"){
+            $(this).parent().addClass("active");
+        }
+        $("#" + target).load("content/" + page + ".html");
+    }
+    event.preventDefault();
+};
+
+function setAge(){
+	$(".craig-age").html(function(){
+        var birthdate = new Date(1988, 9, 26);
+        var today = Date.now();
+        var ageDate = new Date(today - birthdate);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    });
+};
+
+function initPage(){
+	$(".content-change").click(contentChange);
+    setAge();
+};
+
+$(document).ready(initPage);
