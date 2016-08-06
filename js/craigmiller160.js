ad1459fece3bebb12ae80bd9e433ce4eda30fe04
craigmiller160 <craigmiller160@gmail.com>
2016-08-06 16:16:14 -0400

    Finished slideshow function, and refactored all JavaScript code to conform to design patterns and use better scoping

--- js/craigmiller160.js
+++ js/craigmiller160.js
@@ -1,45 +1,135 @@
 /* CraigMiller160 GitHub Page JavaScript */
-var contentChanger = {
-	displayedContent: {},
-	handleChangeEvent: function (event){
-	    if(event.type === "click"){
-	        var page = $(this).attr("page");
-	        var target = $(this).attr("target");
 
-	        contentChanger.change(page, target, $(this));
-	    }
-	    event.preventDefault();
-	},
-	change: function(page, target, node){
-		$("[target='" + target + "']").parent("li").removeClass("active");
+var cm160 = (function(){
 
-		if(node !== undefined && node.parent().prop("nodeName") === "LI"){
-            node.parent().addClass("active");
-        }
+	var contentChanger = (function(){
+		var displayedContent = [];
 
-        if(contentChanger.displayedContent[target] !== page){
-			$("#" + target).fadeOut(200, function(){
-				$("#" + target).load("content/" + page + ".html", function(){
-					$("#" + target).fadeIn(200);
+		function handleChangeEvent (event){
+			if(event.type === "click"){
+		        var page = $(this).attr("page");
+		        var target = $(this).attr("target");
+
+		        change(page, target, $(this)); //TODO how to handle this
+		    }
+		    event.preventDefault();
+		}
+
+		function change (page, target, node){
+			if(node !== null && node !== undefined && node.parent().prop("nodeName") === "LI"){
+				$("[target='" + target + "']").parent("li").removeClass("active");
+	            node.parent().addClass("active");
+	        }
+
+	        if(displayedContent[target] !== page){
+				$("#" + target).fadeOut(200, function(){
+					$("#" + target).load("content/" + page + ".html", function(){
+						$("#" + target).fadeIn(200);
+					});
 				});
-			});
-			contentChanger.displayedContent[target] = page;
-        }
-	}
-};
+				displayedContent[target] = page;
+	        }
+		}
 
-function setAge(){
-	$(".craig-age").html(function(){
-        var birthdate = new Date(1988, 9, 26);
+		function init(){
+			$(".cm160-content-container").each(function(){
+				var page = $(this).attr("defaultPage");
+				change(page, $(this).attr("id"));
+			});
+			$(".cm160-change-content").click(handleChangeEvent);
+		}
+
+		return {
+			"init": init
+		}
+	})();
+
+
+	var slideshow = (function(){
+		var currentIndexMap = [];
+		var imagesDivMap = [];
+
+		function addImage(src, alt, index, name){
+			var img = $("<img/>");
+			img.addClass("cm160-slideshow"); //TODO make this class more unique
+
+			img.attr("src", src);
+			img.attr("alt", alt);
+			$(imagesDivMap[name]).append(img);
+			if(index > 0){
+				$(img).hide();
+			}
+		}
+
+		function preload(imgArray, name){
+			$.each(imgArray, function(index, value){
+				var image = new Image();
+				image.onload = function(){
+					addImage(image.src, value.alt, index, name);
+				};
+				image.onerror = function(){
+					console.log("Error loading image: " + value.src);
+					addImage("", value.alt, index, name);
+				};
+				image.src = value.src;
+			});
+		}
+
+		function cycle(oldIndex, currentIndex) {
+			$(".cm160-slideshow").eq(oldIndex).fadeOut(200, function(){
+				$(".cm160-slideshow").eq(currentIndex).fadeIn(200);
+			});
+		}
+
+		function start(name){
+			currentIndexMap[name] = 0;
+
+			$(".cm160-img-container").each(function(){
+				if(name === this.id){
+					imagesDivMap[name] = this;
+					return false;
+				}
+			});
+
+			if(imagesDivMap[name] === undefined){
+				console.log("No div exists with class 'img-slideshow' and id '" + name + "'");
+				return;
+			}
+
+			$.getJSON("content/slideshow-" + name + ".json", function (data){
+				preload(data.images, name);
+
+				var autoSlide = setInterval(function(){
+					var oldIndex = currentIndexMap[name];
+					currentIndexMap[name] += 1;
+					if (currentIndexMap[name] > data.images.length - 1){
+						currentIndexMap[name] = 0;
+					}
+
+					cycle(oldIndex, currentIndexMap[name]);
+				}, 3000);
+			});
+		}
+
+		return {
+			"start": start
+		}
+
+	})();
+
+
+	function setAge(){
+		var birthdate = new Date(1988, 9, 26);
         var today = Date.now();
         var ageDate = new Date(today - birthdate);
         return Math.abs(ageDate.getUTCFullYear() - 1970);
-    });
-};
+	}
 
-function initPage(){
-	$(".content-change").click(contentChanger.handleChangeEvent);
-    setAge();
-};
 
-$(document).ready(initPage);
+	return {
+		"setAge": setAge,
+		"contentChanger": contentChanger,
+		"slideshow": slideshow
+	}
+
+})();
